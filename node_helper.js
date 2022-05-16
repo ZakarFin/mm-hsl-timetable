ab0d5f6e57508a7c720c616a2715537a727b01a9
Sami Mäkinen <zakarfin@gmail.com>
2022-05-17 00:25:29 +0300

    Formatting

--- node_helper.js
+++ node_helper.js
@@ -3,44 +3,44 @@ const fetch = require("node-fetch");
 var NodeHelper = require("node_helper");
 
 function getSchedule(baseUrl, stop, successCb, errorCB) {
-	const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-	const headers = {
-		"Content-Type": "application/graphql",
-		"User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version,
-		"Cache-Control": "max-age=0, no-cache, no-store, must-revalidate",
-		Pragma: "no-cache"
-	};
+    const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+    const headers = {
+        "Content-Type": "application/graphql",
+        "User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version,
+        "Cache-Control": "max-age=0, no-cache, no-store, must-revalidate",
+        Pragma: "no-cache"
+    };
 
-	fetch(baseUrl, {
+    fetch(baseUrl, {
         method: 'POST',
         body:    getHSLPayload(stop.id || stop, moment().format("YYYYMMDD")),
-		headers: headers })
-		.then(NodeHelper.checkFetchStatus)
-		.then(response => response.json())
-		.then(json => {
-			if (!json.data) {
-				errorCB('No data');
-				return;
-			}
-			const data = json.data.stop;
-			if (!data) {
-				errorCB('No stop data');
-				return;
-			}
-			var response = {
-				stop: stop.id || stop,
-				name: stop.name || data.name,
-				busses: processBusData(data.stoptimesForServiceDate, stop.minutesFrom)
-			};
-			successCb(response);
-		})
-		.catch((error) => {
-			errorCB(error);
-		});
+        headers: headers })
+        .then(NodeHelper.checkFetchStatus)
+        .then(response => response.json())
+        .then(json => {
+            if (!json.data) {
+                errorCB('No data');
+                return;
+            }
+            const data = json.data.stop;
+            if (!data) {
+                errorCB('No stop data');
+                return;
+            }
+            var response = {
+                stop: stop.id || stop,
+                name: stop.name || data.name,
+                busses: processBusData(data.stoptimesForServiceDate, stop.minutesFrom)
+            };
+            successCb(response);
+        })
+        .catch((error) => {
+            errorCB(error);
+        });
 }
 
 function getHSLPayload(stop, date) {
-	return `{
+    return `{
       stop(id: "HSL:${stop}") {
         name
         lat
@@ -70,79 +70,79 @@ function getHSLPayload(stop, date) {
 }
 
 function processBusData(json, minutesFrom = 0) {
-	let times = [];
-	if (!json || json.length < 1) {
-		return times;
-	}
-	json.forEach(value => {
-		if (value && !value.stoptimes) {
-			return;
-		}
-		const line = value.pattern.route.shortName;
-		value.stoptimes.forEach(stopTime => {
-			// times in seconds so multiple by 1000 for ms
-			let datVal = new Date(
-				(stopTime.serviceDay + stopTime.realtimeDeparture) * 1000
-			);
-			if (datVal.getTime() < new Date().getTime() + (minutesFrom * 60 * 1000)) {
-				return;
-			}
-			const date = moment(datVal);
-			const bus = {
-				line,
-				info: stopTime.trip.alerts.join(),
-				time: date.format("H:mm"),
-				until: date.fromNow(),
-				ts: datVal.getTime()
-			};
-			times.push(bus);
-		});
-	});
-	times.sort((a, b) => a.ts - b.ts);
-	return times;
+    let times = [];
+    if (!json || json.length < 1) {
+        return times;
+    }
+    json.forEach(value => {
+        if (value && !value.stoptimes) {
+            return;
+        }
+        const line = value.pattern.route.shortName;
+        value.stoptimes.forEach(stopTime => {
+            // times in seconds so multiple by 1000 for ms
+            let datVal = new Date(
+                (stopTime.serviceDay + stopTime.realtimeDeparture) * 1000
+            );
+            if (datVal.getTime() < new Date().getTime() + (minutesFrom * 60 * 1000)) {
+                return;
+            }
+            const date = moment(datVal);
+            const bus = {
+                line,
+                info: stopTime.trip.alerts.join(),
+                time: date.format("H:mm"),
+                until: date.fromNow(),
+                ts: datVal.getTime()
+            };
+            times.push(bus);
+        });
+    });
+    times.sort((a, b) => a.ts - b.ts);
+    return times;
 }
 
 module.exports = NodeHelper.create({
-	config: {},
-	updateTimer: null,
-	start: function () {
-		moment.locale(config.language || "fi");
-	},
+    config: {},
+    updateTimer: null,
+    start: function () {
+        moment.locale(config.language || "fi");
+    },
 
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "CONFIG") {
-			this.config = payload;
-			this.scheduleNextFetch(this.config.initialLoadDelay);
-		}
-	},
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "CONFIG") {
+            this.config = payload;
+            this.scheduleNextFetch(this.config.initialLoadDelay);
+        }
+    },
 
-	fetchTimetables() {
-		var self = this;
-		this.config.stops.forEach(stop => {
-			getSchedule(
-				this.config.apiURL,
-				stop,
-				data => {
-					self.sendSocketNotification("TIMETABLE", data);
-					self.scheduleNextFetch(this.config.updateInterval);
-				},
-				err => {
-					console.error(err);
-					self.scheduleNextFetch(this.config.retryDelay);
-				}
-			);
-		});
-	},
+    fetchTimetables() {
+        var self = this;
+        this.config.stops.forEach(stop => {
+            getSchedule(
+                this.config.apiURL,
+                stop,
+                data => {
+                    self.sendSocketNotification("TIMETABLE", data);
+                    self.scheduleNextFetch(this.config.updateInterval);
+                },
+                err => {
+                    console.error(err);
+                    self.scheduleNextFetch(this.config.retryDelay);
+                }
+            );
+        });
+    },
 
-	scheduleNextFetch: function (delay) {
-		if (typeof delay === "undefined") {
-			delay = 60 * 1000;
-		}
+    scheduleNextFetch: function (delay) {
+        if (typeof delay === "undefined") {
+            delay = 60 * 1000;
+        }
 
-		var self = this;
-		clearTimeout(this.updateTimer);
-		this.updateTimer = setTimeout(function () {
-			self.fetchTimetables();
-		}, delay);
-	}
+        var self = this;
+        clearTimeout(this.updateTimer);
+        this.updateTimer = setTimeout(function () {
+            self.fetchTimetables();
+        }, delay);
+    }
 });
